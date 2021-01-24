b1ae9ec08fbb8657fc5bf0e62b44cc430b9468b5
tankaishuai <tankaishuai@126.com>
2021-01-24 16:33:37 +0800

    更新至 V70

--- Api_Monitor.cmd
+++ Api_Monitor.cmd
@@ -1,11 +1,17 @@
 @echo off
+setlocal EnableDelayedExpansion
 if "%1" == "" (
 	echo √¸¡Ó––£∫api_monitor.cmd  hex_hwnd  ...
-	pause
+	set /p hwnd=«Î ‰»Î¥∞ø⁄æ‰±˙£∫
+	echo "!hwnd!"
+	if not "!hwnd!" == "" (
+		rundll32 "%cd%\win32exts.dll",MyAnyHookApi_RunDll32 --hwnd=!hwnd!
+	)
 	exit /b 0
 ) else (
 	set pa=%~dp0
-	pushd "%pa%"
-	rundll32 "%cd%\win32exts.dll",MyAnyHookApi_RunDll32 --hwnd=%1 %2
+	pushd "!pa!"
+	echo using "!cd!\win32exts.dll"
+	rundll32 "!cd!\win32exts.dll",MyAnyHookApi_RunDll32 --hwnd=%1 %2
 	popd
 )
